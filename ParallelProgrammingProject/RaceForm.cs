48e64004b114f0825f8643943dae3bdeb32a030c
BrunoFares <faresbruno04@gmail.com>
2025-04-19 20:04:30 +0300

    Started laying out the foundations of the car edits.

--- ParallelProgrammingProject/RaceForm.cs
+++ ParallelProgrammingProject/RaceForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,18 +24,86 @@ namespace ParallelProgrammingProject
 
             List<Car> cars = race.racers;
 
-            int i = 0;
-            foreach (Car car in cars) {
-                Label lab = new();
-                lab.Text = car.Make;
-                lab.Location = new Point(222, 90 + i);
-                lab.AutoSize = true;
-                lab.Font = new Font("Calibri", 14);
-                lab.ForeColor = Color.Green;
-                lab.Padding = new Padding(6);
-                this.Controls.Add(lab);
-                i += 50;
+            for (int i = 0; i < cars.Count; i++) {
+                LoadCar(cars[i], i);
             }
+
+            Button EditButton = new();
+            EditButton.Text = "Edit Car";
+            EditButton.Location = new Point(0, 20 + cars.Count * 90);
+            this.Controls.Add(EditButton);
+
+            Button DoneButton = new();
+            DoneButton.Text = "Apply Edits";
+            DoneButton.Enabled = false;
+            DoneButton.Location = new Point(200, 20 + cars.Count * 90);
+            this.Controls.Add(DoneButton);
+        }
+
+        private void LoadCar(Car car, int index)
+        {
+            Label tag = new();
+            tag.Text = "Contestant " + (index + 1);
+            tag.Location = new Point(0, index * 90);
+            tag.AutoSize = true;
+            tag.Font = new Font("Default", 10, FontStyle.Bold);
+            tag.Padding = new Padding(10, 10, 10, 0);
+            this.Controls.Add(tag);
+
+            TextBox lab1 = new();
+            lab1.Text = car.Make;
+            lab1.Enabled = false;
+            lab1.Location = new Point(0, 20 + index * 90);
+            lab1.AutoSize = true;
+            lab1.Font = new Font("Default", 14);
+            lab1.Padding = new Padding(10, 10, 10, 0);
+            this.Controls.Add(lab1);
+
+            TextBox lab2 = new();
+            lab2.Text = car.Model;
+            lab2.Enabled = false;
+            lab2.Location = new Point(200, 20 + index * 90);
+            lab2.AutoSize = true;
+            lab2.Font = new Font("Default", 14);
+            lab2.Padding = new Padding(10);
+            this.Controls.Add(lab2);
+
+            TextBox lab3 = new();
+            lab3.Text = car.Year.ToString();
+            lab3.Enabled = false;
+            lab3.Location = new Point(0, 60 + index * 90);
+            lab3.AutoSize = true;
+            lab3.Font = new Font("Default", 9);
+            lab3.Padding = new Padding(10, 0, 10, 0);
+            this.Controls.Add(lab3);
+
+            TextBox lab4 = new();
+            lab4.Text = car.HorsePower.ToString();
+            lab4.Enabled = false;
+            lab4.Location = new Point(400, 20 + index * 90);
+            lab4.AutoSize = true;
+            lab4.Font = new Font("Default", 14);
+            lab4.Padding = new Padding(10);
+            this.Controls.Add(lab4);
+
+            TextBox lab5 = new();
+            lab5.Text = car.Weight.ToString();
+            lab5.Enabled = false;
+            lab5.Location = new Point(600, 20 + index * 90);
+            lab5.AutoSize = true;
+            lab5.Font = new Font("Default", 14);
+            lab5.Padding = new Padding(10);
+            this.Controls.Add(lab5);
+        }
+
+        private void EditRows(int index)
+        {
+
+        }
+
+        private void ApplyEdits(int index)
+        {
+
         }
     }
 }
